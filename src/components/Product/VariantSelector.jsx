b0e1c468e15ce33b878965401eb65e704a42dfc5
shimaa <shimaamohamed923@gmail.com>
2026-07-19 15:03:49 +0300

    Improve accessibility

--- src/components/Product/VariantSelector.jsx
+++ src/components/Product/VariantSelector.jsx
@@ -25,7 +25,7 @@ export default function VariantSelector({ product, activeVariant }) {
             type="button"
             onClick={() => handleChangeVariant(variant.id)}
             aria-pressed={activeVariant?.id === variant.id}
-            className={`flex h-6 min-w-16 cursor-pointer items-center justify-center rounded-xs border-[.5px] px-1 text-2xs font-medium text-black-c-900 transition focus:outline-none ${activeVariant?.id === variant.id ? "border-green-c-500 bg-green-c-200/4" : "border-gray-c-400 bg-white "}`}
+            className={`flex h-6 min-w-16 cursor-pointer items-center justify-center rounded-xs border-[.5px] px-1 text-2xs font-medium text-black-c-900 transition focus:outline-none focus-visible:ring-2 focus-visible:ring-purple-c focus-visible:ring-offset-2 ${activeVariant?.id === variant.id ? "border-green-c-500 bg-green-c-200/4" : "border-gray-c-400 bg-white "}`}
           >
             {variant.thumbnail && (
               <span className="max-w-7">
